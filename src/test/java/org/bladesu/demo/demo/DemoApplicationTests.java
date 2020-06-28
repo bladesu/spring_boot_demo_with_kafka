1131c4a6c9912b128d1edd7680aba7db684fdb9e
BladeSu <bladesu@inquartik.com>
2020-06-28 13:29:42 +0800

    Add spring boot components.

---
+++ src/test/java/org/bladesu/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bladesu.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
